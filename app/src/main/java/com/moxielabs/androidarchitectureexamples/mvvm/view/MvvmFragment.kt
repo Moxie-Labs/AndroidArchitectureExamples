fd27190efaab0d3f0fc6b70f5ff9d02c5e8dc195
stevegregos <gregos.steve@gmail.com>
2024-08-07 12:09:21 -0400

    Move ViewHolder

--- app/src/main/java/com/moxielabs/androidarchitectureexamples/mvvm/view/MvvmFragment.kt
+++ app/src/main/java/com/moxielabs/androidarchitectureexamples/mvvm/view/MvvmFragment.kt
@@ -1,16 +1,16 @@
 package com.moxielabs.androidarchitectureexamples.mvvm.view
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.moxielabs.androidarchitectureexamples.R
-import com.moxielabs.androidarchitectureexamples.mvp.view.ViewHolder
+import com.moxielabs.androidarchitectureexamples.shared.view.ViewHolder
 import com.moxielabs.androidarchitectureexamples.mvvm.viewmodel.MvvmViewModel
 
 class MvvmFragment: Fragment() {
@@ -23,11 +23,13 @@ class MvvmFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_architecture, container, false)
+        val recycler = view.findViewById<RecyclerView>(R.id.recycler_view)
+        view.findViewById<View>(R.id.background)?.setBackgroundColor(Color.CYAN)
 
         viewModel = ViewModelProvider(requireActivity())[MvvmViewModel::class.java]
 
-        viewModel.fruits.observe(viewLifecycleOwner, Observer { fruits ->
-            view.findViewById<RecyclerView>(R.id.recycler_view)?.apply {
+        viewModel.fruits.observe(viewLifecycleOwner) { fruits ->
+            recycler?.apply {
                 layoutManager = LinearLayoutManager(this.context)
 
                 adapter = object : RecyclerView.Adapter<ViewHolder>() {
@@ -45,7 +47,7 @@ class MvvmFragment: Fragment() {
                     }
                 }
             }
-        })
+        }
 
 
 
